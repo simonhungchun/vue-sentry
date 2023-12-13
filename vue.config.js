2e18b9e0efc608c9d60ccaaeda6bb4d88cfc05fa
Simon Hungchun <cc_0820@163.com>
2023-12-13 15:10:14 +0800

    配置sentry sourcemap

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     devtool: "source-map",
     plugins: [
       sentryWebpackPlugin({
-        org: "HOPU",
+        org: "hopu",
         project: "vue-sentry",
         authToken:
           "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",
